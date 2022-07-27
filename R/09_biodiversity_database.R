6a0a11e60457e1d00405cba32de18c55a5b11e99
jkuse <julia.kuse97@gmail.com>
2022-07-27 13:04:50 -0300

    biodiversity databases

---
+++ R/09_biodiversity_database.R
@@ -0,0 +1,105 @@
+# biodiversity databases
+
+install.packages("rgbif")
+install.packages("Taxonstand")
+install.packages("CoordinateCleaner")
+install.packages("maps")
+
+library(rgbif)
+library(Taxonstand)
+library(CoordinateCleaner)
+library(maps)
+library(dplyr)
+
+species <- "Myrsine coriacea"
+
+occs <- occ_search(scientificName = species,
+                   limit = 100000)
+names(occs)
+
+myrsine.data <- occs$data
+colnames(myrsine.data)
+
+# dir.create("data/raw/", recursive = TRUE)
+write.csv(myrsine.data,
+          "data/raw/myrsine_data.csv",
+          row.names = FALSE)
+
+sort(unique(myrsine.data$scientificName))
+
+table(myrsine.data$taxonomicStatus)
+
+table(myrsine.data$scientificName, myrsine.data$taxonomicStatus)
+
+# unique vector is used so we don't have duplicated names
+species.names <- unique(myrsine.data$scientificName)
+dim(species.names)
+
+# TPL(): check if the taxonomic updates in the gbif data are correct.
+tax.check <- TPL(species.names)
+tax.check
+
+# creating new object w/ original and new names after TPL
+new.tax <- data.frame(scientificName = species.names,
+                      genus.new.TPL = tax.check$New.Genus,
+                      species.new.TPL = tax.check$New.Species,
+                      status.TPL = tax.check$Taxonomic.status,
+                      scientificName.new.TPL = paste(tax.check$New.Genus,
+                                                     tax.check$New.Species))
+# now we are merging raw data and checked data
+myrsine.new.tax <- merge(myrsine.data, new.tax, by = "scientificName")
+
+# Exporting data after taxonomy check
+#dir.create("data/processed/", recursive = TRUE)
+write.csv(myrsine.new.tax,
+          "data/processed/data_taxonomy_check.csv",
+          row.names = FALSE)
+
+# Checking geographical coordinates
+plot(decimalLatitude ~ decimalLongitude, data = myrsine.data, asp = 1)
+map(, , , add = TRUE)
+
+# clean_coordinates(): clean the species records checks for common
+# errors in coordinates such as institutional coordinates, sea coordinates,
+# outliers, zeros, centroids AND DOES NOT ACCEPT NA
+myrsine.coord <- myrsine.data[!is.na(myrsine.data$decimalLatitude)
+                              & !is.na(myrsine.data$decimalLongitude),]
+
+# output w/ only potential correct coordinates
+geo.clean <- clean_coordinates(x = myrsine.coord,
+                               lon = "decimalLongitude",
+                               lat = "decimalLatitude",
+                               species = "species",
+                               value = "clean")
+par(mfrow = c(1, 2))
+plot(decimalLatitude ~ decimalLongitude, data = myrsine.data, asp = 1)
+map(, , , add = TRUE)
+plot(decimalLatitude ~ decimalLongitude, data = geo.clean, asp = 1)
+map(, , , add = TRUE)
+
+par(mfrow = c(1, 1))
+
+myrsine.new.geo <- clean_coordinates(x = myrsine.coord,
+                                     lon = "decimalLongitude",
+                                     lat = "decimalLatitude",
+                                     species = "species",
+                                     value = "spatialvalid")
+# merging w/ original data
+myrsine.new.geo2 <- merge(myrsine.data, myrsine.new.geo,
+                          all.x = TRUE,
+                          by = "key")
+
+plot(decimalLatitude ~ decimalLongitude, data = myrsine.new.geo, asp = 1)
+map(, , , add = TRUE)
+
+plot(decimalLatitude ~ decimalLongitude, data = geo.clean, asp = 1)
+map(, , , add = TRUE)
+
+write.csv(myrsine.new.geo2,
+          "data/processed/myrsine_coordinate_check.csv",
+          row.names = FALSE)
+# Homework
+# how to use this db to  plot in tmap? how to convert into a shapefile
+# 1. data frame have information in decimal latitude and longitud to geometry (sf file)
+# 2. myrsine_sf (Sf) how to add to tmap?
+# 3. tmap_mode -> try to have an interactive map
